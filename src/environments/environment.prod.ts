90830e77e91517473891dbcee498a3b6efffca26
Kanchan Thareja <kthareja12@gmail.com>
2018-06-26 01:40:13 -0700

    firebase, routing and bootstrap4 added.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+   apiKey: "AIzaSyA3OTCDuRpoRAC1jMHf8wQS_bCEsCq2QuY",
+   authDomain: "oshop-5ad4a.firebaseapp.com",
+   databaseURL: "https://oshop-5ad4a.firebaseio.com",
+   projectId: "oshop-5ad4a",
+   storageBucket: "oshop-5ad4a.appspot.com",
+   messagingSenderId: "263087953885"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+   apiKey: "AIzaSyA3OTCDuRpoRAC1jMHf8wQS_bCEsCq2QuY",
+   authDomain: "oshop-5ad4a.firebaseapp.com",
+   databaseURL: "https://oshop-5ad4a.firebaseio.com",
+   projectId: "oshop-5ad4a",
+   storageBucket: "oshop-5ad4a.appspot.com",
+   messagingSenderId: "263087953885"
+  }
+};
